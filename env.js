e4b94742fd8d232141ba613dbf70788da97e9713
954alberto <alberto.rodriguez.garcia@.ah.nl>
2026-07-21 20:43:55 +0200

    feat: add dev branch configuration
    - CNAME: portal.dev.valydar.com
    - env.js: api.dev.valydar.com
    - All links point to dev domains

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
-// Production environment config
-window.VALYDAR_API_BASE = 'https://api.valydar.com';
+// Non-production environment config
+window.VALYDAR_API_BASE = 'https://api.dev.valydar.com';
 window.VALYDAR_SANDBOX_KEY = '';
